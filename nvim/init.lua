8f8a6532f627e6555a6e258bba81a6f629502f08
Scott Neubauer <scotteneubauer@gmail.com>
2026-06-11 19:40:48 -0500

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+-- set standard options
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.number = true
+
+-- set color scheme
+
+-- show errors on next-line for cursor attention
+
+-- enable code-completion
+
+-- enable lsp
+vim.lsp.enable({"lua_ls"})
+vim.lsp.enable({"clangd"})
+
+-- enable directory tree
+
+-- set custom keymaps
