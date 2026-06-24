b8d2e1ac5ae101648eca01a7a4e35e893bc76b2d
Scott Neubauer <scotteneubauer@gmail.com>
2026-06-23 19:08:53 -0500

    initial commit

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,36 @@
+-- set standard options
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.number = true
+
+-- show errors on next-line for cursor attention
+
+-- enable code-completion
+
+-- enable lsp
+vim.lsp.enable({"lua_ls"})
+vim.lsp.enable({"clangd"})
+
+-- enable directory tree
+
+-- set custom keymaps
+
+-- add transparency plugin
+require("transparent").setup({
+    groups = {
+        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+        'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+        'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+        'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+        'EndOfBuffer'
+    },
+    extra_groups = {},
+    exclude_groups = {},
+    on_clear = function() end
+})
+
+-- set color scheme
+local ok, matugen = pcall(require, 'matugen')
+if ok then matugen.setup() end
