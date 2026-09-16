4ad6310c32651da2d8fc48d27bc80a71b0b7cfae
Scott Neubauer <scotteneubauer@gmail.com>
2026-09-16 08:28:38 -0500

    adding submap feedback

--- dot_config/umbriel/executable_submap-subscription.sh
+++ dot_config/umbriel/executable_submap-subscription.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 umbriel subscribe submap | while read -r line; do
-    SUBMAP=$(echo $line | jq '.data')
+    SUBMAP=$(echo $line | jq -r '.data')
     if [ ! $SUBMAP = "null" ]; then
-        noctalia msg notification-show $SUBMAP
+        PAYLOAD=$(jq --arg key "$SUBMAP" '.[$key]' ~/.config/umbriel/notification-payloads.json)
+        echo "SUBMAP: $SUBMAP"
+        echo "PAYLOAD: $PAYLOAD"
+        #noctalia msg notification-show $PAYLOAD
     fi
 done
