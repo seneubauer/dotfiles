ea6ab292757de7ff2f6ce82e08c638cdda50aac2
Scott Neubauer <scotteneubauer@gmail.com>
2026-09-17 22:04:54 -0500

    hwat2

--- dot_config/wlr-which-key/executable_wlr-which-key-caller.sh
+++ dot_config/wlr-which-key/executable_wlr-which-key-caller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $XDG_CURRENT_DESKTOP == "niri" ]
-    wlr-which-key ~/.config/wlr-which-key/niri-config.yaml
-elif [ $XDG_CURRENT_DESKTOP == "umbriel" ]
-    wlr-which-key ~/.config/wlr-which-key/umbriel-config.yaml
+if [ $XDG_CURRENT_DESKTOP == "niri" ]; then
+    wlr-which-key ~/.config/wlr-which-key/config-niri.yaml
+elif [ $XDG_CURRENT_DESKTOP == "umbriel" ]; then
+    wlr-which-key ~/.config/wlr-which-key/config-umbriel.yaml
 fi
