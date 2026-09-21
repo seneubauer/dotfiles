033fc5e42d6c8bf27b65a678bdc9d49e64e8e5d0
Scott Neubauer <scotteneubauer@gmail.com>
2026-09-21 08:57:58 -0500

    added backer in wwk

--- dot_config/wlr-which-key/executable_notes-publish.sh
+++ dot_config/wlr-which-key/executable_notes-publish.sh
@@ -3,3 +3,4 @@ cd ~/documents/notes/main
 git add .
 git commit -m "auto-publish"
 git push origin main
+noctalia msg notification-show '{"app_name":"Noctalia","summary":"Notes published","body":"All notes published","urgency":"low","timeout_ms":4000,"icon":"circle-check"}'
--- dot_config/wlr-which-key/executable_notes-retrieve.sh
+++ dot_config/wlr-which-key/executable_notes-retrieve.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 cd ~/documents/notes/main
 git pull origin main
+noctalia msg notification-show '{"app_name":"Noctalia","summary":"Notes synced","body":"Note vault synced to remote repository","urgency":"low","timeout_ms":4000,"icon":"circle-check"}'
